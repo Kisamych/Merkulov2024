089dab41a3b7a1c8b5a5d619ef1a7452961a3fd7
Дмитрий <Merkulovdmitry@mail.ru>
2024-08-28 22:20:24 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,23 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[5:])
+print(example[::-1])
+print(example[1:10:2])
+
+name = "Dima"
+print(name)
+name = "Pasha"
+print(name)
+
+date_of_birth = "March 2022"
+print(date_of_birth)
+
+quantity = 12 # Количество
+hours_spent = 1.5 # Потраченные часы
+Course_name = "Python" # Название курса
+print(Course_name,', всего задач:',quantity,'затрачено часов:',hours_spent,', среднее время выполнения',hours_spent/quantity)
+
+name = 'Urban'
+
+
