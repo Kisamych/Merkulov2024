75e1ecac6221375d3c980ba452dc52e247f5fb36
Дмитрий <Merkulovdmitry@mail.ru>
2024-10-08 11:54:59 +0300

    Add files via upload

---
+++ module_3_2.py
@@ -0,0 +1,44 @@
+# Рассылка писем"
+# Создайте функцию send_email, которая принимает 2 обычных аргумента: message(сообщение), recipient(получатель) и 1 обязательно именованный аргумент со значением по умолчанию sender = "university.help@gmail.com".
+def send_email(massage, recipient, *, sender="university.help@gmail.com"): # Обязательно именованные аргументы отделяются от остальных символом "*" перед ними. Именованные аргументы всегда идут после позиционных.
+    a = "@"
+    while 1 > 0:
+# Если строки recipient и sender не содержит "@" или не оканчивается на ".com"/".ru"/".net", то вывести на экран(в консоль) строку: "Невозможно отправить письмо с адреса <sender> на адрес <recipient>".
+        if recipient.endswith(('.com', '.ru', '.net')) and sender.endswith(('.com', '.ru', '.net')):
+            if a not in recipient and sender:
+                print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+                break
+        else:
+            print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
+            break
+# Если же sender и recipient совпадают, то вывести "Нельзя отправить письмо самому себе!"
+        if sender == recipient:
+            print("Нельзя отправить письмо самому себе!")
+            break
+# Если же отправитель по умолчанию - university.help@gmail.com, то вывести сообщение: "Письмо успешно отправлено с адреса <sender> на адрес <recipient>."
+        elif sender == "university.help@gmail.com":
+            print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+            break
+# В противном случае вывести сообщение: "НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса <sender> на адрес <recipient>."
+        elif sender != "university.help@gmail.com":
+            print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+            break
+
+
+send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+
+
+
+
+
+
+
+
+
+
+
+
+
